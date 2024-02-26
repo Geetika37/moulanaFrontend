a574a7782e089f1b283526a687b012ee3b924c0c
Geetika37 <geetikadinesh3@gmail.com>
2024-02-26 15:05:27 +0530

    master created

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:moulanafrontend/pages/LoginPage.dart';
+
+void main()
+{
+  runApp(home());
+}
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
 
---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 90,),
+              Row(
+                children: [
+                  Image(
+                    image: AssetImage('assets/Untitled-edgerszh 2.png'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  Text("Proceed with",style: TextStyle(color: Colors.grey,fontSize: 13),),
+                ],
+              ),
+        
+              SizedBox(height: 10,),
+              Row(
+                children: [
+                  Text("Login",style: TextStyle(color: Colors.teal.shade800,fontSize: 25,fontWeight: FontWeight.bold),),
+                ],
+              ),
+        
+              SizedBox(height: 105,),
+              IntlPhoneField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                  )
+                ),
+              ),
+              SizedBox(height: 15,),
+        
+              SizedBox(
+                width: 290,
+                child: ElevatedButton(onPressed: (){}, child: Text("Continue"),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.teal.shade800,foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
+                ),
+                ),
+              )
+        
+        
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
